72f6bcdffb2a54bdcf3e48395b2b6c134b70fa67
Wendell Liu <cuk.bas@gmail.com>
2018-10-15 23:57:53 +0800

    adts-newtype-instance example code

--- adts-newtype-instance/src/Main.purs
+++ adts-newtype-instance/src/Main.purs
@@ -1,9 +1,24 @@
 module Main where
 
-import Prelude
+import Prelude (class Show, Unit, (<>))
 import Data.Show
+import Effect (Effect)
+import Effect.Console (logShow)
 
-newtype FirstName = FirstName 
+
+newtype FirstName = FirstName String
+newtype LastName = LastName String
+
+data Name = Name { firstName :: FirstName, lastName :: LastName }
+
+name :: Name
+name = Name { firstName: FirstName "Wendell", lastName: LastName "Liu" }
 
 instance showName :: Show Name where
   show (Name {firstName: FirstName f, lastName: LastName l}) = f <> " " <> l
+
+
+
+main :: Effect Unit
+main = do
+  logShow name
